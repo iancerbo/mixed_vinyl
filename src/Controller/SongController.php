7fc580d34a7854dee5f6370bd9e9471b9483519f
Ian Johnson <ian.johnson@cer.bo>
2023-11-05 17:15:19 -0500

    Add services

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController {
-    #[Route('/api/songs/{id}')]
-    public function getSong($id) : Response {
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    public function getSong(int $id, LoggerInterface $logger) : Response {
         $song = [
             'id' => $id,
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3'
         ];
+
+        $logger->info('Returning API response for song {song}', [
+            'song' => $id,
+        ]);
 
         return $this->json($song);
     }
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage() : Response {
+    public function homepage(Environment $twig) : Response {
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
             ['song' => 'Waterfalls', 'artist' => ' TLC'],
@@ -22,10 +23,12 @@ class VinylController extends AbstractController
 //        dd($tracks);
 //        dump($tracks);
 
-        return $this->render('vinyl/homepage.html.twig', [
+        $html = $twig->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks,
         ]);
+
+        return new Response($html);
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
